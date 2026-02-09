2ade3745ef8aac05a05e71acb8ce2c7112b711ea
mainlib990 <43126937+mainlib990@users.noreply.github.com>
2026-02-09 14:29:32 +0900

    feat(content): 스프린트 2 디자인 2 (#69)
    * feat(content): Content 도메인 추가
    
    * feat(content): Content 커맨드 추가
    
    * feat(content): Content 정책 추가
    
    * feat(content): Content 이벤트 추가
    
    * refactor(content): import 최적화

---
+++ src/main/java/org/codeit/sb06/team03/mopl/content/domain/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package org.codeit.sb06.team03.mopl.content.domain;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/org/codeit/sb06/team03/mopl/content/domain/vo/Type.java
@@ -0,0 +1,5 @@
+package org.codeit.sb06.team03.mopl.content.domain.vo;
+
+public enum Type {
+    MOVIE, TV_SERIES, SPORT
+}
