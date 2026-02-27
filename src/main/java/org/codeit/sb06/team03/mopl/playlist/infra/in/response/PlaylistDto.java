18696565fb1a577ff6ed05ac6f1bdbaf515433f4
김도현 <henly5@naver.com>
2026-02-27 11:15:13 +0900

    feat(playlist): contents 임시 추가

--- src/main/java/org/codeit/sb06/team03/mopl/playlist/infra/in/response/PlaylistDto.java
+++ src/main/java/org/codeit/sb06/team03/mopl/playlist/infra/in/response/PlaylistDto.java
@@ -1,9 +1,11 @@
 package org.codeit.sb06.team03.mopl.playlist.infra.in.response;
 
+import org.codeit.sb06.team03.mopl.common.ContentResult;
 import org.codeit.sb06.team03.mopl.playlist.PlaylistReadModel;
 import org.codeit.sb06.team03.mopl.playlist.domain.entity.Playlist;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public record PlaylistDto(
@@ -13,11 +15,11 @@ public record PlaylistDto(
         String description,
         Instant updatedAt,
         long subscriberCount,
-        boolean subscribedByMe
-        //List<ContentDto> contents // TODO
+        boolean subscribedByMe,
+        List<ContentResult> contents // TODO
 ) {
 
-    public static PlaylistDto toDto(Playlist playlist, UserSummaryDto owner, boolean subscribedByMe/*, List<ContentDto> contents*/) {
+    public static PlaylistDto toDto(Playlist playlist, UserSummaryDto owner, boolean subscribedByMe, List<ContentResult> contents) {
         return new PlaylistDto(
                 playlist.getId(),
                 owner,
@@ -25,12 +27,12 @@ public record PlaylistDto(
                 playlist.getDescription(),
                 playlist.getUpdatedAt(),
                 playlist.getSubscriberCount(),
-                subscribedByMe
-                // contents
+                subscribedByMe,
+                contents
         );
     }
 
-    public static PlaylistDto toDto(PlaylistReadModel readModel, UserSummaryDto owner, boolean subscribedByMe/*, List<ContentDto> contents*/) {
+    public static PlaylistDto toDto(PlaylistReadModel readModel, UserSummaryDto owner, boolean subscribedByMe, List<ContentResult> contents) {
         return new PlaylistDto(
                 readModel.id(),
                 owner,
@@ -38,8 +40,8 @@ public record PlaylistDto(
                 readModel.description(),
                 readModel.updatedAt(),
                 readModel.subscriberCount(),
-                subscribedByMe
-                // contents
+                subscribedByMe,
+                contents
         );
     }
 }
